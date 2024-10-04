0a00699759f2b342f59445b2932e94a4f97e3d79
ShahebazKhan04 <pathanshahbazkhan1433@gmail.com>
2024-10-04 15:03:53 +0530

    database formated

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,12 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDb = async () => {
-    try {
-        await mongoose.connect(process.env.MONGOURL + process.env.DBNAME);
-        console.log(`connected to DB successfully ==> ${process.env.MONGOURL + process.env.DBNAME}` )
-    } catch (error) {
-        console.log(`error while connection to db` + error.message)
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGOURL + process.env.DBNAME);
+    console.log(
+      `connected to DB successfully ==> ${
+        process.env.MONGOURL + process.env.DBNAME
+      }`
+    );
+  } catch (error) {
+    console.log(`error while connection to db` + error.message);
+  }
+};
 
 export default connectDb;
